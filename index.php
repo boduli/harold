7f49692a637888c2e6eb24ea4ee42bd4227dff2d
gerald.matege@ehmis.org <>
2012-07-16 14:24:50 +0300

    merged

--- index.php
+++ index.php
@@ -10,5 +10,6 @@ echo "testing hullo";
 print_r("We are testing another Merge Work Flow");
 echo "serious merge testing - netbeans gitplugin";
 echo 'we are doing it again';
-echo 'Will it work finally ?'
+echo 'Will it work finally ? MTX';
+echo 'New Line';
 ?>
